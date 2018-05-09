3dc94a488edc7fd023ff297a79681a95572476dd
bharathkumar874 <bharathkumar874@gmail.com>
2018-05-09 23:49:38 +0530

    The working copy

---
+++ PushNotificationsPOC/PushNotificationsPOC/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using Firebase.Iid;
+
+namespace PushNotificationsPOC
+{
+    [Activity(Label = "PushNotificationsPOC", MainLauncher = true, Icon ="@drawable/notification")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            if (!GetString(Resource.String.google_app_id).Equals("1:800530973596:android:0dd23db5aed7c3cb"))
+            {
+                throw new System.Exception("invalid json file");
+            }
+
+            Task.Run(() =>
+            {
+                var instaneId = FirebaseInstanceId.Instance;
+                instaneId.DeleteInstanceId();
+                Android.Util.Log.Debug("TAG", "{0} {1}", instaneId.Token, instaneId.GetToken(GetString(Resource.String.gcm_defaultSenderId), Firebase.Messaging.FirebaseMessaging.InstanceIdScope));
+
+            });
+        }
+    }
+}
+
---
+++ PushNotificationsPOC/PushNotificationsPOC/MyFirebaseMessagingService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace PushNotificationsPOC
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            SendNotification(message.GetNotification().Body);
+        }
+
+        private void SendNotification(string body)
+        {
+            var bitmap = BitmapFactory.DecodeResource(this.Resources, Resource.Drawable.mylocationmarker);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+            var defaultSoundURI = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                .SetSmallIcon(Resource.Drawable.mylocationplaceholder)
+                .SetLargeIcon(bitmap)
+                .SetContentTitle(GetString(Resource.String.app_name))
+                .SetContentText(body)
+                .SetAutoCancel(true)
+                .SetSound(defaultSoundURI)
+                .SetContentIntent(pendingIntent);
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+
+        }
+    }
+}
